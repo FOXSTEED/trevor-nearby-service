33d18729a365fd8501bbac5545afb33e164acb92
kguinto <klogold1@gmail.com>
2018-02-23 13:58:08 -0800

    Implemented generation of random coordinates within a set of ranges

--- models/seedDb.js
+++ models/seedDb.js
@@ -2,15 +2,17 @@ const Sequelize = require('sequelize');
 //const sequelize = new Sequelize('postgres://localhost:5432/testdb');
 const faker = require('faker');
 const randomPuppy = require('random-puppy');
-
+const randomCoordinates = require('./randomCoordinates')
 
 randomPuppy()
   .then(url => {
+      let coords = randomCoordinates.getRandomCoordinates();
+
       let attraction = {
         attraction_id: 0,
         name: faker.lorem.words(faker.random.number({min:1, max:3})),
-        latitude: faker.address.latitude(),
-        longitude: faker.address.longitude(),
+        latitude: coords.latitude,
+        longitude: coords.longitude,
         address: faker.address.streetAddress(true),
         rating: faker.random.number({min:0, max:5}),
         ranking: faker.random.number({min:0, max:5}),
@@ -23,15 +25,15 @@ randomPuppy()
     console.error(err);
   });
 
-
-
 randomPuppy()
   .then(url => {
+      let coords = randomCoordinates.getRandomCoordinates();
+
       let hotel = {
         hotel_id: 0,
         name: faker.lorem.words(faker.random.number({min:1, max:3})),
-        latitude: faker.address.latitude(),
-        longitude: faker.address.longitude(),
+        latitude: coords.latitude,
+        longitude: coords.longitude,
         address: faker.address.streetAddress(true),
         rating: faker.random.number({min:0, max:5}),
         ranking: faker.random.number({min:0, max:5}),
@@ -44,14 +46,15 @@ randomPuppy()
     console.error(err);
   });
 
-
 randomPuppy()
   .then(url => {
+      let coords = randomCoordinates.getRandomCoordinates();
+
       let restaurant = {
         restaurant_id: 0,
         name: faker.lorem.words(faker.random.number({min:1, max:3})),
-        latitude: faker.address.latitude(),
-        longitude: faker.address.longitude(),
+        latitude: coords.latitude,
+        longitude: coords.longitude,
         address: faker.address.streetAddress(true),
         rating: faker.random.number({min:0, max:5}),
         no_reviews: faker.random.number({min:0, max:10}),
