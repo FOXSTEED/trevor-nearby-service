b529c4d291e1f8cfbdcc042617fc5d481a2900ad
kguinto <klogold1@gmail.com>
2018-02-23 11:50:55 -0800

    Implemented creation of example data for one of each schema

--- models/seedDb.js
+++ models/seedDb.js
@@ -5,8 +5,7 @@ const randomPuppy = require('random-puppy');
 
 
 randomPuppy()
-    .then(url => {
-
+  .then(url => {
       let attraction = {
         attraction_id: 0,
         name: faker.lorem.words(faker.random.number({min:1, max:3})),
@@ -18,6 +17,50 @@ randomPuppy()
         tags: faker.lorem.words(faker.random.number({min:0, max:5})), //stringified array
         image_url: url
       };
-      
       console.log(attraction);
-    })
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
+
+
+randomPuppy()
+  .then(url => {
+      let hotel = {
+        hotel_id: 0,
+        name: faker.lorem.words(faker.random.number({min:1, max:3})),
+        latitude: faker.address.latitude(),
+        longitude: faker.address.longitude(),
+        address: faker.address.streetAddress(true),
+        rating: faker.random.number({min:0, max:5}),
+        ranking: faker.random.number({min:0, max:5}),
+        tags: faker.lorem.words(faker.random.number({min:0, max:5})), //stringified array
+        image_url: url
+      };
+      console.log(hotel);
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
+
+randomPuppy()
+  .then(url => {
+      let restaurant = {
+        restaurant_id: 0,
+        name: faker.lorem.words(faker.random.number({min:1, max:3})),
+        latitude: faker.address.latitude(),
+        longitude: faker.address.longitude(),
+        address: faker.address.streetAddress(true),
+        rating: faker.random.number({min:0, max:5}),
+        no_reviews: faker.random.number({min:0, max:10}),
+        ranking: faker.random.number({min:0, max:5}),
+        tags: faker.lorem.words(faker.random.number({min:0, max:5})), //stringified array
+        image_url: url
+      };
+      console.log(restaurant);
+  })
+  .catch(err => {
+    console.error(err);
+  });
