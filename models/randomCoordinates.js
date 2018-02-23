5ecc4abef6c966e42133f4177ef63d9361ee7c91
kguinto <klogold1@gmail.com>
2018-02-23 12:21:47 -0800

    Refactored to include random coordinates from multiple cities (SF and NY)

--- models/randomCoordinates.js
+++ models/randomCoordinates.js
@@ -5,29 +5,35 @@ const LOCATIONS = {
     min_longitude: -122507387, // -122.507387
     max_latitude: 37807562, // 37.807562
     max_longitude: -122390252 // -122.390252
+  },
+
+  NEWYORK : {
+    min_latitude: 40706023,
+    min_longitude: -74011928,
+    max_latitude: 40757108,
+    max_longitude: -73970028
   }
 
 }
 
 let randomCity = () => {
-  return LOCATIONS.SANFRANCISCO; //todo: randomize this
+  let cities = Object.keys(LOCATIONS);
+
+  let randomIndex = Math.floor(Math.random() * cities.length);
+
+  return LOCATIONS[cities[randomIndex]];
 }
 
-let randomLatitude = (city = null) => {
+let getRandomCoordinates = (city = null) => {
   if(!city) {
     city = randomCity();
   }
 
-  return Math.floor( Math.random() * (city.max_latitude - city.min_latitude) + city.min_latitude) / 1000000;
+  let lat =  Math.floor( Math.random() * (city.max_latitude - city.min_latitude) + city.min_latitude) / 1000000;
+
+  let long = Math.floor( Math.random() * (city.max_longitude - city.min_longitude) + city.min_longitude) / 1000000;
+
+  return {latitude: lat, longitude: long};
 };
 
-let randomLongitude = (city = null) => {
-  if(!city) {
-    city = randomCity();
-  }
-
-  return Math.floor( Math.random() * (city.max_longitude - city.min_longitude) + city.min_longitude) / 1000000;
-};
-
-console.log(randomLatitude());
-console.log(randomLongitude());
+console.log(getRandomCoordinates());
