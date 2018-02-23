f52b8435a8d71b00752a93f9a7c771774b5b82c9
kguinto <klogold1@gmail.com>
2018-02-22 19:36:51 -0800

    Implemented script to create a table in Postgres using Sequelize

---
+++ models/database.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('postgres://localhost:5432/testdb');
+
+
+
+const Restaraunt = sequelize.define('restaurant', {
+  restaurants_id: Sequelize.INTEGER,
+  latitude: Sequelize.DOUBLE,
+  longitude: Sequelize.DOUBLE,
+  address: Sequelize.STRING,
+  rating: Sequelize.INTEGER,
+  no_reviews: Sequelize.INTEGER,
+  ranking: Sequelize.INTEGER,
+  tags: Sequelize.STRING, //stringified array
+  image_url: Sequelize.STRING
+
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .then(() => {
+    Restaraunt.sync();
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
