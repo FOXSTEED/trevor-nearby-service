b0bc009cbaadc5949f1f43d1a8253f3b1f1335b4
kguinto <klogold1@gmail.com>
2018-02-27 13:11:48 -0800

    Bubble resizing

--- client/reviewEmptyBubble.jsx
+++ client/reviewEmptyBubble.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 const ReviewEmptyBubble = () => (
-  <svg width="20" height="20" viewBox="0 0 100 100">
+  <svg width="16" height="16" viewBox="10 10 80 80">
     <ellipse fillOpacity="0" ry="35" rx="35" id="svg_1" cy="50" cx="50" strokeWidth="8" stroke="#00a680" fill="#000000" />
   </svg>
 );
