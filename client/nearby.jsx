29e32e8a005143a6b81c038519ac43d1a48495b7
kguinto <klogold1@gmail.com>
2018-03-01 22:07:14 -0800

    Refactored to stateless component and receive data on render

--- client/nearby.jsx
+++ client/nearby.jsx
@@ -1,31 +1,21 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
-import { render } from 'react-dom';
 import NearbyItems from './nearbyItems';
 
-class Nearby extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="nearby">
-        <div className="block-header">
-          <div className="block-title">
-            Nearby
-          </div>
-        </div>
-        <div className="map-block">
-          <div className="nearby-map" />
-        </div>
-        <NearbyItems type="Hotel" />
-        <NearbyItems type="Restaurant" />
-        <NearbyItems type="Attraction" />
+const Nearby = props => (
+  <div className="nearby">
+    <div className="block-header">
+      <div className="block-title">
+        Nearby
       </div>
-    );
-  }
-}
+    </div>
+    <div className="map-block">
+      <div className="nearby-map" />
+    </div>
+    <NearbyItems type="Hotel" items={props.hotels} />
+    <NearbyItems type="Restaurant" items={props.restaurants} />
+    <NearbyItems type="Attraction" items={props.attractions} />
+  </div>
+);
 
-render(<Nearby />, document.getElementById('nearby'));
+export default Nearby;
