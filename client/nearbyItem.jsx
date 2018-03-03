360d0c03f01b6c1f687d438381f531a3c99b2ef5
kguinto <klogold1@gmail.com>
2018-03-03 15:07:03 -0800

    Implemented passing of rating to review bubble component

--- client/nearbyItem.jsx
+++ client/nearbyItem.jsx
@@ -8,7 +8,7 @@ const NearbyItem = props => (
     <div className="item-column">
       <div className="item-name">{props.item.name}</div>
       <div className="item-reviews">
-        <ReviewBubbles />
+        <ReviewBubbles rating={props.item.rating} />
         <div className="review-count">42 reviews</div>
       </div>
       <div className="item-distance">123 miles away</div>
