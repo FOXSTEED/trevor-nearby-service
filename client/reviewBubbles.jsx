b6b13d9ebb76523bae34bcf5556bc79c2772528a
kguinto <klogold1@gmail.com>
2018-03-03 15:07:59 -0800

    Implemented dynamic rendering of bubbles based on rating

--- client/reviewBubbles.jsx
+++ client/reviewBubbles.jsx
@@ -4,14 +4,32 @@ import ReviewBubble from './reviewBubble';
 import ReviewHalfBubble from './reviewHalfBubble';
 import ReviewEmptyBubble from './reviewEmptyBubble';
 
-const ReviewBubbles = () => (
-  <div className="review-bubbles">
-    <ReviewBubble />
-    <ReviewBubble />
-    <ReviewBubble />
-    <ReviewHalfBubble />
-    <ReviewEmptyBubble />
-  </div>
-);
+let lastKey = -1;
+const newKey = () => {
+  lastKey += 1;
+  return lastKey;
+};
+
+const ReviewBubbles = (props) => {
+  let fullBubbleCount = Math.floor(props.rating / 2);
+  let halfBubbleCount = props.rating % 2;
+  let emptyBubbleCount = 5 - (fullBubbleCount + halfBubbleCount);
+
+  return (
+    <div className="review-bubbles">
+      {(new Array(fullBubbleCount))
+        .fill('')
+        .map(() => <ReviewBubble key={newKey()} />)}
+
+      {(new Array(halfBubbleCount))
+        .fill('')
+        .map(() => <ReviewHalfBubble key={newKey()} />)}
+
+      {(new Array(emptyBubbleCount))
+        .fill('')
+        .map(() => <ReviewEmptyBubble key={newKey()} />)}
+    </div>
+  );
+};
 
 export default ReviewBubbles;
