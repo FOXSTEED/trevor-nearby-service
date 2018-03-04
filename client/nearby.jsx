356d94481a484af9b885d17e93d6c8aa4d1458b3
kguinto <klogold1@gmail.com>
2018-03-03 16:00:56 -0800

    Refactored to component class and implemented method to calculate distance from the page's attraction

--- client/nearby.jsx
+++ client/nearby.jsx
@@ -2,20 +2,54 @@
 import React from 'react';
 import NearbyItems from './nearbyItems';
 
-const Nearby = props => (
-  <div className="nearby">
-    <div className="block-header">
-      <div className="block-title">
-        Nearby
+const distance = require('gps-distance');
+const kmToMi = require('km-to-mi');
+
+class Nearby extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getDistance = this.getDistance.bind(this);
+  }
+
+  getDistance(latitude, longitude) {
+    let km = distance(
+      this.props.attraction.latitude,
+      this.props.attraction.longitude,
+      latitude,
+      longitude
+    );
+    return Math.floor(kmToMi(km));
+  }
+
+  render() {
+    return (
+      <div className="nearby">
+        <div className="block-header">
+          <div className="block-title">
+            Nearby
+          </div>
+        </div>
+        <div className="map-block">
+          <div className="nearby-map" />
+        </div>
+        <NearbyItems
+          type="Hotel"
+          items={this.props.hotels}
+          getDistance={this.getDistance}
+        />
+        <NearbyItems
+          type="Restaurant"
+          items={this.props.restaurants}
+          getDistance={this.getDistance}
+        />
+        <NearbyItems
+          type="Attraction"
+          items={this.props.attractions}
+          getDistance={this.getDistance}
+        />
       </div>
-    </div>
-    <div className="map-block">
-      <div className="nearby-map" />
-    </div>
-    <NearbyItems type="Hotel" items={props.hotels} />
-    <NearbyItems type="Restaurant" items={props.restaurants} />
-    <NearbyItems type="Attraction" items={props.attractions} />
-  </div>
-);
+    );
+  }
+}
 
 export default Nearby;
