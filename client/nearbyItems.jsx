690f2433db81cc97e515597a553a32a5f2859145
kguinto <klogold1@gmail.com>
2018-03-03 16:02:54 -0800

    Added passing of getDistance function to each item

--- client/nearbyItems.jsx
+++ client/nearbyItems.jsx
@@ -6,7 +6,12 @@ const NearbyItems = props => (
   <div className="nearby-items">
     <div className="section-title">Nearby {props.type}s</div>
     {props.items.map(item => (
-      <NearbyItem key={JSON.stringify(item)} type={props.type} item={item} />
+      <NearbyItem
+        key={JSON.stringify(item)}
+        type={props.type}
+        item={item}
+        getDistance={props.getDistance}
+      />
     ))}
   </div>
 );
