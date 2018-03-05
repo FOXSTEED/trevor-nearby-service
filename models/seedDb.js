b98dfa99a65099c0d0dcefd94a50d89ad697d6f2
kguinto <klogold1@gmail.com>
2018-03-05 10:25:51 -0800

    Renamed 'no_reviews' field to 'num_reviews'

--- models/seedDb.js
+++ models/seedDb.js
@@ -15,7 +15,7 @@ for (let i = 0; i < 200; i += 1) {
         longitude: coords.longitude,
         address: faker.address.streetAddress(true),
         rating: faker.random.number({ min: 0, max: 10 }),
-        no_reviews: faker.random.number({ min: 0, max: 10 }),
+        num_reviews: faker.random.number({ min: 0, max: 10 }),
         ranking: faker.random.number({ min: 0, max: 5 }),
         tags: faker.lorem.words(faker.random.number({ min: 0, max: 5 })), // stringified array
         image_url: url
@@ -38,7 +38,7 @@ for (let i = 0; i < 200; i += 1) {
         longitude: coords.longitude,
         address: faker.address.streetAddress(true),
         rating: faker.random.number({ min: 0, max: 10 }),
-        no_reviews: faker.random.number({ min: 0, max: 10 }),
+        num_reviews: faker.random.number({ min: 0, max: 10 }),
         ranking: faker.random.number({ min: 0, max: 5 }),
         tags: faker.lorem.words(faker.random.number({ min: 0, max: 5 })), // stringified array
         image_url: url
@@ -61,7 +61,7 @@ for (let i = 0; i < 200; i += 1) {
         longitude: coords.longitude,
         address: faker.address.streetAddress(true),
         rating: faker.random.number({ min: 0, max: 10 }),
-        no_reviews: faker.random.number({ min: 0, max: 10 }),
+        num_reviews: faker.random.number({ min: 0, max: 10 }),
         ranking: faker.random.number({ min: 0, max: 5 }),
         tags: faker.lorem.words(faker.random.number({ min: 0, max: 5 })), // stringified array
         image_url: url
