0efdc019de20b62fba838aaffb5e52d0c62b4605
kguinto <klogold1@gmail.com>
2018-03-05 11:38:24 -0800

    Refactor to render React component with just id as prop, and get nearby item data from inside react component

--- client/index.jsx
+++ client/index.jsx
@@ -1,18 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Nearby from './nearby';
-import dh from './dataHelpers';
 
 const url = require('url');
-
-let getData = async (id) => {
-  let attraction = dh.getItem('attractions', id);
-  let hotels = dh.getNearest('hotels', id);
-  let restaurants = dh.getNearest('restaurants', id);
-  let attractions = dh.getNearest('attractions', id);
-
-  return Promise.all([id, attraction, hotels, restaurants, attractions]);
-};
 
 let id =
   parseInt(
@@ -22,13 +12,7 @@ let id =
     , 10
   );
 
-getData(id)
-  .then((data) => {
-    render(<Nearby
-      id={data[0]}
-      attraction={data[1]}
-      hotels={data[2]}
-      attractions={data[3]}
-      restaurants={data[4]}
-    />, document.getElementById('nearby'));
-  });
+render(<Nearby
+  id={id}
+/>, document.getElementById('nearby'));
+
--- client/nearby.jsx
+++ client/nearby.jsx
@@ -2,19 +2,45 @@
 import React from 'react';
 import NearbyItems from './nearbyItems';
 
+const { getItem, getNearest } = require('./dataHelpers');
 const distance = require('gps-distance');
 const kmToMi = require('km-to-mi');
+
+const getData = async (id) => {
+  let attraction = getItem('attractions', id);
+  let hotels = getNearest('hotels', id);
+  let restaurants = getNearest('restaurants', id);
+  let attractions = getNearest('attractions', id);
+
+  return Promise.all([attraction, hotels, restaurants, attractions]);
+};
 
 class Nearby extends React.Component {
   constructor(props) {
     super(props);
     this.getDistance = this.getDistance.bind(this);
+    this.state = {
+      attraction: {},
+      hotels: [],
+      restaurants: [],
+      attractions: []
+    };
+
+    getData(props.id)
+      .then(((data) => {
+        this.setState({
+          attraction: data[0],
+          hotels: data[1],
+          restaurants: data[2],
+          attractions: data[3]
+        });
+      }));
   }
 
   getDistance(latitude, longitude) {
     let km = distance(
-      this.props.attraction.latitude,
-      this.props.attraction.longitude,
+      this.state.attraction.latitude,
+      this.state.attraction.longitude,
       latitude,
       longitude
     );
@@ -34,17 +60,17 @@ class Nearby extends React.Component {
         </div>
         <NearbyItems
           type="Hotel"
-          items={this.props.hotels}
+          items={this.state.hotels}
           getDistance={this.getDistance}
         />
         <NearbyItems
           type="Restaurant"
-          items={this.props.restaurants}
+          items={this.state.restaurants}
           getDistance={this.getDistance}
         />
         <NearbyItems
           type="Attraction"
-          items={this.props.attractions}
+          items={this.state.attractions}
           getDistance={this.getDistance}
         />
       </div>
