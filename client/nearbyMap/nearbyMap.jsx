80ae3a018cb4aa6b1076f023e2e1796765d2f334
kguinto <klogold1@gmail.com>
2018-03-05 19:06:28 -0800

    Implemented nearby map component

---
+++ client/nearbyMap/nearbyMap.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable react/prop-types, no-unused-vars, no-undef */
+import React from 'react';
+import loadJS from './loadJS';
+import GOOGLE_MAPS_API_KEY from './googlemaps.config';
+
+class NearbyMap extends React.Component {
+  componentDidMount() {
+    // Google Maps' script will be loaded onto the DOM as a script tag and will run this function,
+    //   so it's needed on the global scope
+    window.initializeMap = this.initializeMap.bind(this);
+    loadJS(`https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&callback=initializeMap`);
+  }
+
+  initializeMap() {
+    let attraction = { lat: this.props.attraction.latitude, lng: this.props.attraction.longitude };
+    let map = new google.maps.Map(window.document.getElementById('map'), {
+      zoom: 4,
+      center: attraction
+    });
+    let marker = new google.maps.Marker({
+      position: attraction,
+      map
+    });
+  }
+
+  render() {
+    return (
+      <div className="nearby-map">
+        <div id="map" />
+      </div>
+    );
+  }
+}
+
+export default NearbyMap;
