e2c4713382bf88873301c9e838e6fa79c1f0f4a9
kguinto <klogold1@gmail.com>
2018-03-06 21:26:50 -0800

    Small refactor to use sequelize instance from models.js

--- models/initDb.js
+++ models/initDb.js
@@ -1,11 +1,6 @@
-const Sequelize = require('sequelize');
-const { Restaurant, Hotel, Attraction } = require('./models.js');
-
-const DBHOST = process.env.DBHOST || 'localhost';
-const DBPORT = process.env.DBPORT || 5432;
-const DBNAME = process.env.DBNAME || 'nearbyitems';
-
-const sequelize = new Sequelize(`postgres://${DBHOST}:${DBPORT}/${DBNAME}`, { logging: false });
+const {
+  sequelize, Hotel, Restaurant, Attraction
+} = require('./models.js');
 
 let promises = [];
 
@@ -20,7 +15,7 @@ sequelize
   })
   .then(async () => {
     await Promise.all(promises);
-    console.log(`Created ${promises.length} tables at postgres://${DBHOST}:${DBPORT}/${DBNAME}`);
+    console.log(`Created ${promises.length} tables.`);
     sequelize.close();
   })
   .catch((err) => {
