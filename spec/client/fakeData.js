e1df9132c41f3b1d02e59c06631340fc89cea04d
kguinto <klogold1@gmail.com>
2018-03-07 20:39:14 -0800

    Created hardcoded fake data for testing

---
+++ spec/client/fakeData.js
@@ -0,0 +1,196 @@
+
+let attraction = {
+  attraction_id: 43,
+  name: 'Karishire',
+  latitude: 37.727716,
+  longitude: -122.417351,
+  address: '4586 McCullough Rapid Suite 393',
+  rating: 10,
+  num_reviews: 3,
+  ranking: 1,
+  tags: 'nesciunt inventore consequuntur omnis quasi',
+  image_url: 'http://gle2e78pzo-flywheel.netdna-ssl.com/wp-content/uploads/2017/04/MIssionSF.jpg',
+  createdAt: '2018-03-07T01:17:37.675Z',
+  updatedAt: '2018-03-07T01:17:37.675Z'
+};
+
+let hotels = [
+  {
+    hotel_id: 44,
+    name: 'Lake Sigmund',
+    latitude: 37.799607,
+    longitude: -122.405544,
+    address: '491 Alfreda Villages Suite 502',
+    rating: 0,
+    num_reviews: 5,
+    ranking: 0,
+    tags: 'veritatis doloribus ipsam',
+    image_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlrVueftXqtL8hoAe8i41tRuopHbm-xO115ae1pedqF9alCTFx',
+    createdAt: '2018-03-07T01:17:37.675Z',
+    updatedAt: '2018-03-07T01:17:37.675Z'
+  },
+  {
+    hotel_id: 45,
+    name: 'South Elmore',
+    latitude: 37.761515,
+    longitude: -122.487386,
+    address: '1448 Jamison Ways Apt. 328',
+    rating: 0,
+    num_reviews: 1,
+    ranking: 2,
+    tags: 'rerum',
+    image_url: 'https://s-ec.bstatic.com/images/hotel/max1024x768/485/48597575.jpg',
+    createdAt: '2018-03-07T01:17:37.678Z',
+    updatedAt: '2018-03-07T01:17:37.678Z'
+  },
+  {
+    hotel_id: 46,
+    name: 'Lake Marina',
+    latitude: 37.77499,
+    longitude: -122.392761,
+    address: '6301 Smitham Station Suite 356',
+    rating: 8,
+    num_reviews: 1,
+    ranking: 1,
+    tags: 'itaque ad suscipit et',
+    image_url: 'https://s-ec.bstatic.com/images/hotel/max1024x768/360/36092062.jpg',
+    createdAt: '2018-03-07T01:17:37.680Z',
+    updatedAt: '2018-03-07T01:17:37.680Z'
+  },
+  {
+    hotel_id: 47,
+    name: 'New Agustinville',
+    latitude: 37.779296,
+    longitude: -122.498175,
+    address: '8313 Schinner Landing Apt. 353',
+    rating: 6,
+    num_reviews: 8,
+    ranking: 3,
+    tags: '',
+    image_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROR3uEp_sVh6zxht9g-4bGpOHiCn5_sZckNW08jScwAJwMUjZetw',
+    createdAt: '2018-03-07T01:17:37.682Z',
+    updatedAt: '2018-03-07T01:17:37.682Z'
+  }
+];
+
+let restaurants = [
+  {
+    restaurant_id: 44,
+    name: 'Hubertville',
+    latitude: 37.716724,
+    longitude: -122.453433,
+    address: '34809 Manuel Falls Suite 133',
+    rating: 10,
+    num_reviews: 1,
+    ranking: 5,
+    tags: 'rerum adipisci enim omnis numquam',
+    image_url: 'https://media.timeout.com/images/103258037/image.jpg',
+    createdAt: '2018-03-07T01:08:39.887Z',
+    updatedAt: '2018-03-07T01:08:39.887Z'
+  },
+  {
+    restaurant_id: 45,
+    name: 'North Paolomouth',
+    latitude: 37.737793,
+    longitude: -122.444499,
+    address: '2667 Madilyn Parkway Suite 216',
+    rating: 9,
+    num_reviews: 0,
+    ranking: 2,
+    tags: 'in voluptatem',
+    image_url: 'https://d37219swed47g7.cloudfront.net/media/reviews/greens-restaurant/banners/greens-sf_0.jpg',
+    createdAt: '2018-03-07T01:08:39.890Z',
+    updatedAt: '2018-03-07T01:08:39.890Z'
+  },
+  {
+    restaurant_id: 46,
+    name: 'West Jacechester',
+    latitude: 37.80648,
+    longitude: -122.415767,
+    address: '97990 Welch Hollow Suite 057',
+    rating: 0,
+    num_reviews: 7,
+    ranking: 3,
+    tags: 'et',
+    image_url: 'https://media.timeout.com/images/103425975/image.jpg',
+    createdAt: '2018-03-07T01:08:39.890Z',
+    updatedAt: '2018-03-07T01:08:39.890Z'
+  },
+  {
+    restaurant_id: 47,
+    name: 'North Shanny',
+    latitude: 37.768389,
+    longitude: -122.417453,
+    address: '7318 Schowalter Well Suite 203',
+    rating: 10,
+    num_reviews: 0,
+    ranking: 0,
+    tags: 'perferendis in qui adipisci',
+    image_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzm2Wh4mLHC2A1JY02RAwgpIY7A3_9MDsmezkFWcaikZZLuim-GQ',
+    createdAt: '2018-03-07T01:08:39.891Z',
+    updatedAt: '2018-03-07T01:08:39.891Z'
+  }
+];
+
+let attractions = [
+  {
+    attraction_id: 44,
+    name: 'West Elsechester',
+    latitude: 37.746754,
+    longitude: -122.502554,
+    address: '20434 Kathleen Field Suite 111',
+    rating: 0,
+    num_reviews: 10,
+    ranking: 3,
+    tags: 'qui et',
+    image_url: 'https://img1.10bestmedia.com/Images/Photos/232069/p-IMG-0762_54_990x660_201404241344.jpg',
+    createdAt: '2018-03-07T01:17:37.678Z',
+    updatedAt: '2018-03-07T01:17:37.678Z'
+  },
+  {
+    attraction_id: 45,
+    name: 'Feltonhaven',
+    latitude: 37.713711,
+    longitude: -122.460937,
+    address: '8707 Wisozk Meadow Suite 642',
+    rating: 7,
+    num_reviews: 2,
+    ranking: 2,
+    tags: 'assumenda et omnis nam quia',
+    image_url: 'https://media.timeout.com/images/101714755/image.jpg',
+    createdAt: '2018-03-07T01:17:37.679Z',
+    updatedAt: '2018-03-07T01:17:37.679Z'
+  },
+  {
+    attraction_id: 46,
+    name: 'North Pansy',
+    latitude: 37.765281,
+    longitude: -122.392263,
+    address: '69041 Nikolas Knoll Apt. 403',
+    rating: 10,
+    num_reviews: 8,
+    ranking: 3,
+    tags: 'enim aut expedita adipisci',
+    image_url: 'http://cdn.mntm.me/60/2c/a0/Best-Things-to-Do-with-Kids-from-San-Diego-to-San-Francisco-602ca00f77854755ac30f4da7862ddaf.jpg',
+    createdAt: '2018-03-07T01:17:37.681Z',
+    updatedAt: '2018-03-07T01:17:37.681Z'
+  },
+  {
+    attraction_id: 47,
+    name: 'Port Deon',
+    latitude: 37.784118,
+    longitude: -122.43043,
+    address: '623 Klein Extensions Apt. 760',
+    rating: 3,
+    num_reviews: 8,
+    ranking: 5,
+    tags: 'nostrum sunt rerum ipsa ea',
+    image_url: 'https://media.timeout.com/images/101714767/image.jpg',
+    createdAt: '2018-03-07T01:17:37.682Z',
+    updatedAt: '2018-03-07T01:17:37.682Z'
+  }
+];
+
+module.exports = {
+  attraction, hotels, restaurants, attractions
+};
