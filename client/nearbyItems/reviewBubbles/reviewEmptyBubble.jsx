389dd1f9709662abedce140f3e30500e9e810c7e
kguinto <klogold1@gmail.com>
2018-03-07 20:33:24 -0800

    Added className

--- client/nearbyItems/reviewBubbles/reviewEmptyBubble.jsx
+++ client/nearbyItems/reviewBubbles/reviewEmptyBubble.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 const ReviewEmptyBubble = () => (
-  <svg width="16" height="16" viewBox="10 10 80 80">
+  <svg className="reviewEmptyBubble" width="16" height="16" viewBox="10 10 80 80">
     <ellipse fillOpacity="0" ry="35" rx="35" id="svg_1" cy="50" cx="50" strokeWidth="8" stroke="#00a680" fill="#000000" />
   </svg>
 );
