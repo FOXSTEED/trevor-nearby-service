16cbc4252688a0dcab836d51095cc2e431274209
Trevor Old <trevor.old@outlook.com>
2018-03-28 19:48:05 -0700

    clean up and enabling of airbnb style

--- performance/artillery/artillery-config.js
+++ performance/artillery/artillery-config.js
@@ -3,28 +3,28 @@ const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'));
 
 
-let randomNum = (userContext, events, done) =>{
-  const id = faker.random.number({ min: 1, max: 9000000 })
+const randomNum = (userContext, events, done) => {
+  const id = faker.random.number({ min: 1, max: 9000000 });
   userContext.vars.id = id;
   return done();
-} 
+};
 
-let printNthousandRandomNumsToCsv =  (n) => {
+const printNthousandRandomNumsToCsv = (n) => {
   for (let i = 0; i < n; i += 1) {
-    var id = faker.random.number({ min: 1, max: 9000000 });
+    let id = faker.random.number({ min: 1, max: 9000000 });
     console.log(id);
     if (i === 0) {
       fs.appendFileAsync('./payload.csv', `id\n${id},\n`)
-      .then()
-      .catch(error => console.log(error));
+        .then()
+        .catch(error => console.log(error));
     }
-      fs.appendFileAsync('./payload.csv', `${id},\n`)
+    fs.appendFileAsync('./payload.csv', `${id},\n`)
       .then()
       .catch(error => console.log(error));
-  };
-  console.log(`Done`);
-}
+  }
+  console.log('Done');
+};
 // printNthousandRandomNumsToCsv(5000)
 module.exports = {
-  randomNum
-}
+  randomNum,
+};
--- server/server.js
+++ server/server.js
@@ -1,33 +1,29 @@
 require('newrelic');
 const morgan = require('morgan');
 const express = require('express');
-const React = require('react');
 const path = require('path');
-const { renderToString } = require('react-dom/server');
-const { Restaurant, Hotel, Attraction } = require('../models/models.js');
 const { getAttraction, getHotel, getRestaurant } = require('../models/rawPg/pgModels');
 const cors = require('cors');
-const template = require('./template');
-const Nearby = require('../public/app-server.js')
+
 require('dotenv').config();
 
 
 const app = express();
 const PORT = process.env.PORT || 3003;
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
 app.use(cors());
-// app.use(express.static(path.join(__dirname, '../public')));
+
 app.use(express.static(path.join(__dirname, '../public')));
-// app.use('/nearby/bundle.js', express.static('public/build/bundle.js'));
+
 
 app.use('/:id', express.static('public/'));
 
 
 /**
- *   
- * 1	attraction
+ *
+ * 1 attraction
  * 2 hotel
  * 3 restaurant
 //  */
@@ -36,12 +32,12 @@ app.get('/nearby/restaurants/:id', (req, res) => {
   if (req.params.id) {
     getRestaurant(req.params.id)
       .then((data) => {
-        res.send(data)
+        res.send(data);
       })
       .catch((err) => {
         console.log(err);
         res.status(400).send();
-      })    
+      });
   }
 });
 
@@ -49,12 +45,12 @@ app.get('/nearby/hotels/:id', (req, res) => {
   if (req.params.id) {
     getHotel(req.params.id)
       .then((data) => {
-        res.send(data)
+        res.send(data);
       })
       .catch((err) => {
         console.log(err);
         res.status(400).send();
-      })    
+      });
   }
 });
 
@@ -62,12 +58,12 @@ app.get('/nearby/attractions/:id', (req, res) => {
   if (req.params.id) {
     getAttraction(req.params.id)
       .then((data) => {
-        res.send(data)
+        res.send(data);
       })
       .catch((err) => {
         console.log(err);
         res.status(400).send();
-      })    
+      });
   }
 });
 
